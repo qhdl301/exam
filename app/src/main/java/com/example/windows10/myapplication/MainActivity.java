3aaa78747362d8d26c1ef260ed696dd22207d1e6
Young soo Kim <qhdl301@gmail.com>
2016-12-09 11:06:19 +0900

    어플리케이션 생성 및 레이아웃 인원예약 생성

--- app/src/main/java/com/example/windows10/myapplication/MainActivity.java
+++ app/src/main/java/com/example/windows10/myapplication/MainActivity.java
@@ -9,16 +9,18 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Switch swt1;
     Chronometer ch1;
-    Button bt1,bt2,bt3;
+    Button bt2,bt3;
     EditText edt1,edt2,edt3,edt4,edt5,edt6;
     RadioGroup rbg1;
     RadioButton rb1,rb2,rb3;
     ImageView img1;
+    TextView tx1;
 
 
 
@@ -27,8 +29,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tx1=(TextView)findViewById(R.id.textView12);
         swt1=(Switch)findViewById(R.id.switch1);
         ch1=(Chronometer)findViewById(R.id.chronometer3);
-        bt1=(Button)findViewById(R.id.button);
+        bt2=(Button)findViewById(R.id.button2);
+        bt3=(Button)findViewById(R.id.button3);
+        edt1=(EditText)findViewById(R.id.editText);
+        edt2=(EditText)findViewById(R.id.editText2);
+        edt3=(EditText)findViewById(R.id.editText3);
+        edt4=(EditText)findViewById(R.id.editText4);
+        edt5=(EditText)findViewById(R.id.editText5);
+        edt6=(EditText)findViewById(R.id.editText6);
+        rbg1=(RadioGroup)findViewById(R.id.radioGroup);
+        rb1=(RadioButton)findViewById(R.id.radioButton4);
+        rb2=(RadioButton)findViewById(R.id.radioButton5);
+        rb3=(RadioButton)findViewById(R.id.radioButton6);
+        img1=(ImageView)findViewById(R.id.imageView);
+
     }
 }
